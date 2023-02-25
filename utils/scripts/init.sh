ce5fb094bb12a539e33b6585565467a263420efe
DimaAmega <dima.amega1@gmail.com>
2023-02-25 09:25:55 +0400

    better

--- utils/scripts/init.sh
+++ utils/scripts/init.sh
@@ -3,7 +3,6 @@ set -e
 
 PLATFORM=$(uname)
 INIT_SCRIPT_URL="https://raw.githubusercontent.com/DimaAmega/web-fundamentals/main/utils/scripts/init.js"
-INIT_SCRIPT_NAME=tini.js
 
 if [[ "$PLATFORM" == 'Linux' ]]; then
     (apt update && apt upgrade -y) || (sudo apt update && sudo apt upgrade -y) || true
@@ -17,6 +16,4 @@ then
     (npm install -g n) || (sudo npm install -g n)
 fi
 
-curl $INIT_SCRIPT_URL > $INIT_SCRIPT_NAME
-node $INIT_SCRIPT_NAME
-rm -rf $INIT_SCRIPT_NAME
+curl -s $INIT_SCRIPT_URL | node
