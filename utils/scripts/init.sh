9506887100b088d1143a05bb4265bad80ffefff0
Dmitry Khorkin <32310771+DimaAmega@users.noreply.github.com>
2023-03-14 19:26:08 +0400

    fix init.sh

--- utils/scripts/init.sh
+++ utils/scripts/init.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 set -e
 
+exec_sudo () {
+    if command -v sudo &> /dev/null
+    then
+        eval "sudo $1"
+    else
+        eval $1
+    fi
+}
+
 PLATFORM=$(uname)
 INIT_SCRIPT_URL="https://raw.githubusercontent.com/DimaAmega/web-fundamentals/main/utils/scripts/init.js"
 
 if [[ "$PLATFORM" == 'Linux' ]]; then
-    (apt update && apt upgrade -y) || (sudo apt update && sudo apt upgrade -y) || true
-    type -p curl >/dev/null || apt install curl -y || sudo apt install curl -y
-    type -p git >/dev/null || apt install git -y || sudo apt install git -y
+    exec_sudo "apt update" && exec_sudo "apt upgrade -y"
+    type -p curl >/dev/null || exec_sudo "apt install curl -y"
+    type -p git >/dev/null ||  exec_sudo "apt install git -y"
 fi
 
 if ! command -v n &> /dev/null
 then
-    curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s lts
-    (npm install -g n) || (sudo npm install -g n)
+    exec_sudo "curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s lts"
+    exec_sudo "npm install -g n"
 fi
 
-curl -fsSL $INIT_SCRIPT_URL | node
+exec_sudo "curl -fsSL $INIT_SCRIPT_URL | node"
